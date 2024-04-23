78db84bbe4daca15e94e367e9014ea7f77a52f27
ryusei-igarashi-mt <ryusei.igarashi@metateam.co.jp>
2024-04-24 00:11:48 +0900

    貸出一覧と登録画面の追加

--- src/main/java/jp/co/metateam/library/controller/RentalManageController.java
+++ src/main/java/jp/co/metateam/library/controller/RentalManageController.java
@@ -64,7 +64,6 @@ public class RentalManageController {
 
     @GetMapping("/rental/add")
     public String add(Model model) {
-        List <RentalManage> rentalManageList = this.rentalManageService.findAll();
         List <Account> accountList = this.accountService.findAll();
         List <Stock> stockList = this.stockService.findAll();
 
